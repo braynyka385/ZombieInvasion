9b576a6e20f45469c5d6c938b1a6391761a88193
Brayden Nykamp <braynyka385@outlook.com>
2023-03-19 21:17:18 -0400

    Add project files.

---
+++ ZombieInvasion/Entity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZombieInvasion
+{
+    public abstract class Entity
+    {
+        protected double x, y;
+        protected double health;
+        protected double maxSpeed;
+        protected double xSpeed, ySpeed;
+
+        public abstract bool Attack(Entity target);
+    }
+
+    public class Player : Entity
+    {
+        private int ammoCount;
+        double money;
+        
+        public Player(double _x, double _y, double _health, double _maxSpeed, double _money, int _ammoCount)
+        {
+            x = _x;
+            y= _y;
+            health= _health;
+            maxSpeed= _maxSpeed;
+            money= _money;
+            ammoCount = _ammoCount;
+            
+        }
+        public override bool Attack(Entity target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ZombieInvasion/MenuScreen.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ZombieInvasion
+{
+    partial class MenuScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.playButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.optionsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("PMingLiU-ExtB", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(75, 114);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(430, 48);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "BLAH BLAH BLAH";
+            // 
+            // playButton
+            // 
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(32, 403);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(159, 104);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // quitButton
+            // 
+            this.quitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quitButton.Location = new System.Drawing.Point(407, 403);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(159, 104);
+            this.quitButton.TabIndex = 2;
+            this.quitButton.Text = "Quit";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // optionsButton
+            // 
+            this.optionsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionsButton.Location = new System.Drawing.Point(221, 403);
+            this.optionsButton.Name = "optionsButton";
+            this.optionsButton.Size = new System.Drawing.Size(159, 104);
+            this.optionsButton.TabIndex = 3;
+            this.optionsButton.Text = "Options";
+            this.optionsButton.UseVisualStyleBackColor = true;
+            // 
+            // MenuScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.Controls.Add(this.optionsButton);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "MenuScreen";
+            this.Size = new System.Drawing.Size(600, 600);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Button optionsButton;
+    }
+}
